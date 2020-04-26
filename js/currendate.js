db1697483650030079fd0048c6d1eccf58162f17
Isaac Rodriguez <rod18056@byui.edu>
2020-04-26 12:43:06 -0500

    padding footer

--- js/currendate.js
+++ js/currendate.js
@@ -1,2 +1,6 @@
 const options = {weekday: 'long', day: 'numeric', month: 'long', year:'numeric'};
 document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', options);
+
+var d = new Date();
+var n = d.getFullYear();
+document.getElementById("currentYear").innerHTML = n;
