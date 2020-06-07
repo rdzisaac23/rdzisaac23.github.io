03343d45dd52df11a23598c1969eba4f790cc594
Isaac Rodriguez <rod18056@byui.edu>
2020-06-06 20:02:19 -0500

    windchill lesson 6

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,11 +1,9 @@
 
 const tempNumber = parseFloat(document.getElementById("temp").textContent);
 const speedNumber = parseFloat(document.getElementById("wind-speed").textContent);
-
 let windchill = Math.round(35.74 + (0.6215 * tempNumber) - (35.75 * Math.pow(speedNumber, 0.16)) + (0.4274 *
     tempNumber * Math.pow(speedNumber, 0.16)));
 console.log(windchill);
-
 if (tempNumber <= 50 && speedNumber > 3) {
     document.getElementById("chill").textContent = "Wind Chill is " + windchill + "\xB0F";
 } else {
